582214e8a76c970e549eafc61fe604d6bfc89671
miliee205 <102649670+miliee205@users.noreply.github.com>
2022-05-14 16:26:31 +0300

    Add files via upload

---
+++ sum5.c
@@ -0,0 +1,10 @@
+//program to find sum of five numbers
+#include<stdio.h>
+int main()
+{
+	int a=10, b=30, c=50, d=60, e=70, sum;
+	sum=a+b+c+d+e;
+	printf("Sum of five numbers is: %d", sum);
+	return 0;
+}
+
